9238eb56b31e8e336d533eb2a020eceedd723a1b
YunTaeSeong <xoqkf1234@naver.com>
2025-12-29 02:46:31 +0900

    common 모듈 - common-core 로 변경

---
+++ common-core/src/main/java/com/moodly/common/domain/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.moodly.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @CreatedBy
+    @Column(nullable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
---
+++ common-core/src/main/java/com/moodly/common/exception/ErrorCode.java
@@ -0,0 +1,6 @@
+package com.moodly.common.exception;
+
+public interface ErrorCode {
+    String code();
+    String message();
+}
