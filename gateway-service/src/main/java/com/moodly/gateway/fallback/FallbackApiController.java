f8ca0d80e74e1489693e9ec33febff98c1b3966c
YunTaeSeong <xoqkf1234@naver.com>
2026-05-14 19:14:30 +0900

    moodly-api: - circuitbreaker fallback 추가

---
+++ gateway-service/src/main/java/com/moodly/gateway/fallback/FallbackApiController.java
@@ -0,0 +1,47 @@
+package com.moodly.gateway.fallback;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackApiController {
+
+    @GetMapping("/auth")
+    public ResponseEntity<String> authFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("auth 서비스가 일시적으로 불안정합니다.");
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<String> productFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("product 서비스가 일시적으로 불안정합니다.");
+    }
+
+    @GetMapping("/order")
+    public ResponseEntity<String> orderFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("order 서비스가 일시적으로 불안정합니다.");
+    }
+
+    @GetMapping("/payment")
+    public ResponseEntity<String> paymentFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("payment 서비스가 일시적으로 불안정합니다.");
+    }
+
+    @GetMapping("/coupon")
+    public ResponseEntity<String> couponFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("coupon 서비스가 일시적으로 불안정합니다.");
+    }
+}
