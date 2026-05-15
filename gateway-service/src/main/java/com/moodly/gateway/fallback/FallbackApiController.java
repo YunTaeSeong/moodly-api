5c9f98516cb9635934f201b6462bd8b8e1af108a
YunTaeSeong <xoqkf1234@naver.com>
2026-05-15 21:50:31 +0900

    moodly-api: - FallbackApiController: auth-service -> circuitbreaker fallbackUri: forward:/fallback/auth Get요청만 받아서 POST 요청시 405 에러 - 모든 요청 받도록 GetMapiing -> RequestMapping 변경 및 RequestMethod 추가

--- gateway-service/src/main/java/com/moodly/gateway/fallback/FallbackApiController.java
+++ gateway-service/src/main/java/com/moodly/gateway/fallback/FallbackApiController.java
@@ -4,13 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackApiController {
 
-    @GetMapping("/auth")
+    @RequestMapping(value = "/auth", method = {
+            RequestMethod.GET, RequestMethod.POST,
+            RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH
+    })
     public ResponseEntity<String> authFallback() {
         return ResponseEntity
                 .status(HttpStatus.SERVICE_UNAVAILABLE)
