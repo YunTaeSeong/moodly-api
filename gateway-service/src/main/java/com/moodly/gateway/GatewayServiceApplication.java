e90f5f0831e2127d9f01f8f4a3fb3564d2230326
YunTaeSeong <xoqkf1234@naver.com>
2026-05-15 21:33:07 +0900

    moodly-api: - Reids RateLimiter : Key Resolver 추가

--- gateway-service/src/main/java/com/moodly/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/moodly/gateway/GatewayServiceApplication.java
@@ -3,6 +3,10 @@ package com.moodly.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication(scanBasePackages = "com.moodly")
 @ConfigurationPropertiesScan(basePackages = {
@@ -11,5 +15,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 }
